429633291b9d722d08f57a6eedd428e8a3509dc9
josscmorera <63267272+josscmorera@users.noreply.github.com>
2023-04-05 18:21:10 -0400

    fin

---
+++ main.js
@@ -0,0 +1,104 @@
+let form = document.querySelector('#todo-form');
+let input = document.querySelector('#todo-input');
+let list = document.querySelector('#todo-list');
+let clearButton = document.querySelector('#clear-button');
+let tasks = [];
+
+
+if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.forEach(task => {
+        addTaskToDOM(task);
+    });
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    let task = {
+        id: Date.now(),
+        text: input.value,
+        status: 'new'
+    };
+    tasks.push(task);
+    addTaskToDOM(task);
+    saveTasksToLocalStorage();
+    input.value = '';
+});
+
+list.addEventListener('change', event => {
+    let taskId = Number(event.target.parentElement.getAttribute('data-task-id'));
+    let taskIndex = tasks.findIndex(task => task.id === taskId);
+    tasks[taskIndex].status = getStatusFromCheckbox(event.target);
+    saveTasksToLocalStorage();
+});
+
+list.addEventListener('click', event => {
+    if (event.target.classList.contains('delete')) {
+        let taskId = Number(event.target.parentElement.getAttribute('data-task-id'));
+        let taskIndex = tasks.findIndex(task => task.id === taskId);
+        tasks.splice(taskIndex, 1);
+        event.target.parentElement.remove();
+        saveTasksToLocalStorage();
+    }
+});
+
+clearButton.addEventListener('click', () => {
+    tasks = [];
+    list.innerHTML = '';
+    localStorage.removeItem('tasks');
+});
+
+function addTaskToDOM(task) {
+    let item = document.createElement('li');
+    item.setAttribute('data-task-id', task.id);
+    let status = document.createElement('input');
+    status.type = 'checkbox';
+    status.checked = task.status === 'completed';
+    status.className = 'status';
+    let text = document.createElement('span');
+    text.innerText = task.text;
+    let statusText = document.createElement('span');
+    statusText.innerText = getStatusText(task.status);
+    statusText.className = 'status-text';
+    let deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    deleteButton.className = 'delete';
+    item.appendChild(status);
+    item.appendChild(text);
+    item.appendChild(statusText);
+    item.appendChild(deleteButton);
+    list.appendChild(item);
+}
+
+function saveTasksToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function getStatusFromCheckbox(checkbox) {
+    if (checkbox.checked) {
+        return 'completed';
+    } else {
+        let statusText = checkbox.nextElementSibling;
+        if (statusText.innerText === 'new') {
+            return 'in-progress';
+        } else {
+            return 'new';
+        }
+    }
+}
+
+function getStatusText(status) {
+    switch (status) {
+        case 'new':
+            return 'new';
+        case 'in-progress':
+            return 'in progress';
+        case 'completed':
+            return 'completed';
+        default:
+            return '';
+    }
+} 
+
+
+
